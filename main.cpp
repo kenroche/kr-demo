97b7302a5e7db2710fdf7a5a1f2d42c9d1a1c053
Kenneth Roche <kennethroche@Kenneths-MacBook-Air.local>
2026-06-11 02:46:02 -0700

    Add fibonacci() to math utilities

--- main.cpp
+++ main.cpp
@@ -5,5 +5,6 @@ int main() {
     std::cout << "kr-demo math utilities\n";
     std::cout << "5! = " << mathutils::factorial(5) << "\n";
     std::cout << "gcd(48, 36) = " << mathutils::gcd(48, 36) << "\n";
+    std::cout << "fib(10) = " << mathutils::fibonacci(10) << "\n";
     return 0;
 }
